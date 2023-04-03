c89e6b02a001b86ddf27b4466ca93cb4fb98b08f
Julio <72631506+JulioOli@users.noreply.github.com>
2023-04-03 12:13:15 -0300

    início do projeto da pokedex

---
+++ src/pages/Home.jsx
@@ -0,0 +1,34 @@
+import { Grid } from '@mui/material';
+import { Container } from '@mui/system';
+import React from 'react';
+import Navbar from "../components/Navbar";
+import PokemonCard from "../components/PokemonCard"
+
+export const Home = () => {
+    return(
+        <div>
+            <Navbar />
+            <Container maxWidth="false">
+                <Grid container>
+                    <Grid item xs={3}>
+                        <PokemonCard />
+                    </Grid>
+
+                    <Grid item xs={3}>
+                        <PokemonCard />
+                    </Grid>
+
+                    <Grid item xs={3}>
+                        <PokemonCard />
+                    </Grid>
+
+                    <Grid item xs={3}>
+                        <PokemonCard />
+                    </Grid>
+                    
+                </Grid>
+
+            </Container>
+        </div>
+    );
+};
