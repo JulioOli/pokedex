9202aa2a76954373f96063859d681d1ae7e25916
Julio <72631506+JulioOli@users.noreply.github.com>
2023-04-03 12:46:30 -0300

    Pegando a API, fazendo uma promise e atualizando o card de pokemon para começar a  usar os dados da API

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,38 @@
 import { Grid } from '@mui/material';
 import { Container } from '@mui/system';
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import Navbar from "../components/Navbar";
 import PokemonCard from "../components/PokemonCard"
 
 export const Home = () => {
+    const[pokemons, setPokemons] = useState([]);
+
+    useEffect(() => {
+        getPokemons();
+    }, []) //esse useEffect vai chamar o "getPokemons" quando ele for montado e atualizado
+//obs: esse []  no final é pra chamar só quando for montado
+
+
+    const getPokemons = () => {
+         //essa é a URL com os 151 primeiros pokemons da API que eu estou usando
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+    .then((res) => console.log(res) //no fim quando a promise for "cumprida", o then me retorna o que eu pedi da API
+    .catch((err) => console.log(err))) //caso acontecer algum problema, a promise retorna um erro
+    }
+
+   
     return(
         <div>
             <Navbar />
             <Container maxWidth="false">
                 <Grid container>
-                    <Grid item xs={3}>
-                        <PokemonCard />
-                    </Grid>
 
+                {pokemons.map((pokemon) => (
                     <Grid item xs={3}>
-                        <PokemonCard />
-                    </Grid>
-
-                    <Grid item xs={3}>
-                        <PokemonCard />
-                    </Grid>
-
-                    <Grid item xs={3}>
-                        <PokemonCard />
-                    </Grid>
+                    <PokemonCard />
+                </Grid>
+                ))}
                     
                 </Grid>
 
