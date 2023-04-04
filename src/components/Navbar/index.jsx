8d5520468763209ebef1901afbb7d23e4f0d3103
Julio <72631506+JulioOli@users.noreply.github.com>
2023-04-04 20:22:26 -0300

    versão até onde eu consegui fazer da pokedex

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,51 +1,48 @@
-import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
+import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
 
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
   borderRadius: theme.shape.borderRadius,
   backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
+  "&:hover": {
     backgroundColor: alpha(theme.palette.common.white, 0.25),
   },
   marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
     marginLeft: theme.spacing(1),
-    width: 'auto',
+    width: "auto",
   },
 }));
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
+const SearchIconWrapper = styled("div")(({ theme }) => ({
   padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
+  color: "inherit",
+  "& .MuiInputBase-input": {
     padding: theme.spacing(1, 1, 1, 0),
     // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
       },
     },
   },
@@ -53,35 +50,30 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 
 export default function Navbar() {
   return (
-    <Box sx={{ flexGrow: 1, marginBottom:"2em"}}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
+    <Box
+      sx={{
+        flexGrow: 1,
+        marginBottom: "2em",
+        backgroundImage: "linear-gradient(diagonally, red, black, white)",
+      }}
+    >
+      <AppBar
+        position="static"
+        sx={{
+          background: "rgb(255,60,60)",
+          background:
+            "linear-gradient(90deg, rgba(255,60,60,1) 33%, rgba(69,56,56,1) 100%, rgba(255,255,255,1) 100%, rgba(255,253,253,1) 100%)",
+        }}
+      >
+        <Toolbar style={{}}>
           <Typography
             variant="h6"
             noWrap
             component="div"
-            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
           >
-            MUI
+            Pokedex do Julião
           </Typography>
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </Search>
         </Toolbar>
       </AppBar>
     </Box>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,42 +1,57 @@
-import { Grid } from '@mui/material';
-import { Container } from '@mui/system';
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import { Grid } from "@mui/material";
+import { Container } from "@mui/system";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
-import PokemonCard from "../components/PokemonCard"
+
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 
 export const Home = () => {
-    const[pokemons, setPokemons] = useState([]);
+  const [pokemons, setPokemons] = useState([]);
 
-    useEffect(() => {
-        getPokemons();
-    }, []) //esse useEffect vai chamar o "getPokemons" quando ele for montado e atualizado
-//obs: esse []  no final é pra chamar só quando for montado
+  useEffect(() => {
+    getPokemons();  
+  }, []); //esse useEffect vai chamar o "getPokemons" quando ele for montado e atualizado
+  //obs: esse []  no final é pra chamar só quando for montado
 
-
-    const getPokemons = () => {
-         //essa é a URL com os 151 primeiros pokemons da API que eu estou usando
-    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
-    .then((res) => setPokemons(res.data.results) //no fim quando a promise for "cumprida", o then me retorna o que eu pedi da API
-    .catch((err) => console.log(err))) //caso acontecer algum problema, a promise retorna um erro
+  const getPokemons = () => {
+    //essa é a URL com os 151 primeiros pokemons da API que eu estou usando
+    let endpoints = [];
+    for (let int = 1; int <= 151; int++) {
+      endpoints.push(`https://pokeapi.co/api/v2/pokemon/${int}/`);
     }
+    const response = axios.all(endpoints.map((endpoint) => axios.get(endpoint))).then((res) => setPokemons(res)) //no fim quando a promise for "cumprida", o then me retorna o que eu pedi da API
+      .catch((err) => console.log(err)); //caso acontecer algum problema, a promise retorna um erro
+      return response;
+    };
 
-   
-    return(
-        <div>
-            <Navbar />
-            <Container maxWidth="false">
-                <Grid container>
-
-                {pokemons.map((pokemon) => (
-                    <Grid item xs={3}>
-                    <PokemonCard />
-                </Grid>
-                ))}
-                    
-                </Grid>
-
-            </Container>
-        </div>
+  function renderCard(name, key, img) {
+    return (
+      <Card key={key} sx={{ maxWidth: 345, margin: "1rem" }}>
+        <img src={img} alt={name} />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {name}
+          </Typography>
+        </CardContent>
+      </Card>
     );
+  }
+
+  return (
+    <div>
+      <Navbar />
+      <Container maxWidth="false">
+        <Grid container>
+          {pokemons.map((pokemon) => (
+            <Grid item xs={3}>
+              {renderCard(pokemon.data.name, `pokemon-${pokemon.data.name}`, pokemon.data.sprites.front_default)}
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </div>
+  );
 };
